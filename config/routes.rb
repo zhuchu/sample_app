49e7ef7808cc06a4fd4caae1b59cb2e04ae731db
Chu Zhu <chuzhu@Chus-MacBook-Pro-2.local>
2018-01-09 22:45:15 +0100

    create first pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  get 'about' => 'pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
